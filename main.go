e81dd58968904a962512a40cf14ca10826e852ff
sathitsak <sathitsak@gmail.com>
2024-04-20 15:31:09 +0700

    refactor: make env are load from file instead

--- main.go
+++ main.go
@@ -1,30 +1,24 @@
 package main
 
 import (
-	"github.com/labstack/echo/v4"
+	"fmt"
+	"log"
 	"net/http"
 	"os"
-	"fmt"
+
+	"github.com/joho/godotenv"
+	"github.com/labstack/echo/v4"
 	"github.com/sathitsak/assessment-tax/pkg/tax"
 )
 
+
 func main() {
+	err := godotenv.Load()
+  if err != nil {
+    log.Fatal("Error loading .env file")
+  }
 	port := os.Getenv("PORT")
-    if port == "" {
-        port = "8000"
-    }
-	dbURL := os.Getenv("DATABASE_URL")
-	if dbURL == "" {
-        dbURL = "localhost:5432"
-    }
-	adminUsername := os.Getenv("ADMIN_USERNAME")
-	if adminUsername == "" {
-        adminUsername = "adminTax"
-    }
-	adminPassword := os.Getenv("ADMIN_USERNAME")
-	if adminPassword == "" {
-        adminPassword = "admin!"
-    }
+	
 	e := echo.New()
 	
 	e.POST("/tax/calculations", calTaxHandler)
